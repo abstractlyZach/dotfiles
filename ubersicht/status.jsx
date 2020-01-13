33debc5e2c6b023b659c736065c1151811091bf7
Zach Lee <zach3lee@gmail.com>
2020-01-12 17:10:01 -0800

    Update status module to my liking

--- ubersicht/status.jsx
+++ ubersicht/status.jsx
@@ -1,5 +1,17 @@
+let refreshFrequencyInSeconds = 30; 
+
+var dayNames = [
+    "Sun",
+    "Mon",
+    "Tues",
+    "Weds",
+    "Thurs",
+    "Fri",
+    "Sat"
+];
+
 export const command = "pmset -g batt | tail -n 1 | awk '{print $3; print $4}'";
-export const refreshFrequency = 100;
+export const refreshFrequency = refreshFrequencyInSeconds * 1000; // milliseconds
 
 export const className = {
   top: 10,
@@ -14,6 +26,21 @@ function pad(num, size) {
     var s = num+"";
     while (s.length < size) s = "0" + s;
     return s;
+}
+
+function getDate(now) {
+	let dayName = dayNames[now.getDay()];
+	let year = now.getFullYear();
+	let month = pad(now.getMonth() + 1, 2);// lol 0-based indexing on months
+	let date = pad(now.getDate(), 2);
+	return `${dayName} ${year}-${month}-${date}`;
+}
+
+function getTime(now) {
+	let hours = pad(now.getHours(), 2);
+	let minutes = pad(now.getMinutes(), 2);
+	let seconds = pad(now.getSeconds(), 2);
+	return `${hours}:${minutes}.${seconds}`;
 }
 
 export const render = ({output, error}) => {
@@ -35,12 +62,8 @@ export const render = ({output, error}) => {
   let desc = res[1].slice(0, -1)
 
   let now = new Date();
-
-  let time = pad(now.getHours(), 2) + ":" + pad(now.getMinutes(), 2);
-
-  //let date = now.getFullYear() + "-" + (pad(now.getMonth() + 1, 2)) + "-" + pad(now.getDate(), 2);
-  let date = new Date().toLocaleString()
-
+  let time = getTime(now);
+  let date = getDate(now);
 
   return [<div style={style}>{desc}: {power}</div>, <div style={style}>{date}</div>, <div style={style}>{time}</div>]
 }
